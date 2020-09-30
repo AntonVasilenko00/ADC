0f7ddd0bbfb22d6cc71ae9e9c2f8c411eaf5b993
Anton <anton.vasilenko00@gmail.com>
2020-09-30 11:56:29 +0300

    finishing footer with dropdown link

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,3 +4,10 @@ function toggleSearchDropdown() {
 	document.getElementById("search-field").focus();
 	document.getElementById("search-field").select();
 }
+
+function otherServicesToggle() {
+	document.getElementById("other-services-dropdown-content").classList.toggle("active");
+	const p = document.getElementById("last-plus");
+	p.innerHTML == "+" ? (p.innerHTML = "-") : (p.innerHTML = "+");
+	document.getElementById("last-divider").classList.toggle("hide");
+}
